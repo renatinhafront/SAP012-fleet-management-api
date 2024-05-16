dd45a77a057ac565cc4ffd3ee4df132e4adc66a4
renatinhafront <re.saraiva09@hotmail.com>
2024-05-15 23:17:36 -0300

    add o modo de exibição do endpoint

--- src/main/java/com/renatinha/fleetmanagement/entities/Taxi.java
+++ src/main/java/com/renatinha/fleetmanagement/entities/Taxi.java
@@ -1,4 +1,4 @@
-package com.renatinha.fleetmanagement.model;
+package com.renatinha.fleetmanagement.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -12,7 +12,4 @@ public class Taxi {
     private int id;
     private String plate;
 
-
-    public Taxi() {
-    }
 }
