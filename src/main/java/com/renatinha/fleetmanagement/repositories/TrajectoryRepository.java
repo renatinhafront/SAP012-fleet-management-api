47ba7d8a0408817daf3690f39dd43af7866c0eec
renatinhafront <re.saraiva09@hotmail.com>
2024-05-17 09:23:34 -0300

    repository criado

--- src/main/java/com/renatinha/fleetmanagement/repositories/TrajectoryRepository.java
+++ src/main/java/com/renatinha/fleetmanagement/repositories/TrajectoryRepository.java
@@ -1,7 +1,16 @@
 package com.renatinha.fleetmanagement.repositories;
 
 import com.renatinha.fleetmanagement.entities.Trajectory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface TrajectoryRepository extends PagingAndSortingRepository<Trajectory, Long> {
+import java.time.LocalDateTime;
+
+public interface TrajectoryRepository extends PagingAndSortingRepository<Trajectory, Integer> {
+    Page<Trajectory> findAllByTaxiIdAndDateBetween(
+            Integer taxiId,
+            LocalDateTime startDate,
+            LocalDateTime endDate,
+            Pageable pageable);
 }
