ead6992633e7fdd660943cbfa11be0750e7f28e1
renatinhafront <re.saraiva09@hotmail.com>
2024-05-17 09:23:55 -0300

    response criado

--- src/main/java/com/renatinha/fleetmanagement/responses/PageResponse.java
+++ src/main/java/com/renatinha/fleetmanagement/responses/PageResponse.java
@@ -3,7 +3,6 @@ package com.renatinha.fleetmanagement.responses;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.util.Collection;
 import java.util.List;
 
 //builder = construção do obj de forma facilitada
@@ -12,5 +11,5 @@ import java.util.List;
 public class PageResponse {
     private Long totalElements;
     private Integer totalPages;
-    private Iterable content;
+    private List<?> content;
 }
