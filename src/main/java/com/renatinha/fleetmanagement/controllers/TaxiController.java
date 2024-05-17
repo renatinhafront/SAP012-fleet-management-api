da0c517e39b3ab281f7874f90ceed1758e759871
renatinhafront <re.saraiva09@hotmail.com>
2024-05-17 09:29:58 -0300

    endpoint e swagger de taxi ok

--- src/main/java/com/renatinha/fleetmanagement/controllers/TaxiController.java
+++ src/main/java/com/renatinha/fleetmanagement/controllers/TaxiController.java
@@ -2,9 +2,12 @@ package com.renatinha.fleetmanagement.controllers;
 
 import com.renatinha.fleetmanagement.entities.Error;
 import com.renatinha.fleetmanagement.entities.Taxi;
-import com.renatinha.fleetmanagement.responses.PageResponse;
+import com.renatinha.fleetmanagement.entities.Trajectory;
+import com.renatinha.fleetmanagement.responses.*;
 import com.renatinha.fleetmanagement.services.TaxiService;
+import com.renatinha.fleetmanagement.services.TrajectoryService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -15,24 +18,30 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
+import java.time.LocalDateTime;
+import java.util.List;
 
-
+//é um controller onde cada método retorna um objeto de domínio diretamente para o corpo da resposta HTTP.
 @RestController
+//Define o caminho base para todos os métodos neste controller, que é /taxis
 @RequestMapping("/taxis")
 public class TaxiController {
 
     //criando um atributo e/ou instanciando a classe service
     private final TaxiService taxiService;
+    private final TrajectoryService trajectoryService;
 
 
     //construtor
-    public TaxiController(TaxiService taxiService) {
+    public TaxiController(TaxiService taxiService, TrajectoryService trajectoryService) {
         this.taxiService = taxiService;
+        this.trajectoryService = trajectoryService;
     }
 
 
-    @Operation(summary = "Busca uma lista com todos os taxis", method = "GET")
+    @Operation(
+            summary = "Busca uma lista com todos os taxis",
+            method = "GET")
     @ApiResponses(value = {
             @ApiResponse(
                     responseCode = "200",
@@ -57,13 +66,76 @@ public class TaxiController {
 
         //declarando uma váriavel
         Page<Taxi> pages = taxiService.getAllTaxis(pageable);
+
+        List<TaxiResponse> taxiResponses = pages.getContent().stream()
+                .map(t -> TaxiResponse.builder()
+                        .id(t.getId())
+                        .plate(t.getPlate())
+                        .build())
+                .toList();
+
+
         // dto para mapear os campos
         return ResponseEntity.ok(PageResponse.builder()
                 .totalElements(pages.getTotalElements())
                 .totalPages(pages.getTotalPages())
-                .content(Collections.singletonList(pages.getContent()))
+                .content(taxiResponses)
                 //construa este objeto
                 .build()
         );
+    }
+
+
+    @Operation(
+            summary = "Busca trajetórias por ID do táxi",
+            method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Operação com sucesso",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = PageResponse.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Parametros inválidos",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Error.class)
+                    )
+            ),
+    })
+
+    //O endpoint responde para o id do táxi e uma data consultado as seguintes informações:
+    // latitude, longitude e timestamp (data e hora).
+
+    @GetMapping("/{id}/trajectories")
+    public ResponseEntity<PageResponse> getTrajectoriesByTaxiId(
+            @PathVariable @Parameter (example = "6418") Integer id,
+            @Parameter(example = "2008-02-02T00:00:00") LocalDateTime startDate,
+            @Parameter(example = "2008-02-02T23:59:59") LocalDateTime endDate,
+            @ParameterObject Pageable pageable) {
+
+        // trajetórias por ID do táxi
+        Page<Trajectory> pages = trajectoryService.getTrajectoriesByTaxiId(id, startDate, endDate, pageable);
+
+        List<TrajectoryResponse> trajectoryResponses = pages.getContent().stream()
+                .map(t -> TrajectoryResponse.builder()
+                        .dateTime(t.getDate())
+                        .longitude(t.getLongitude())
+                        .latitude(t.getLatitude())
+                        .build())
+                .toList();
+
+        // Construir a resposta com PageResponse genérico
+        PageResponse response = PageResponse.builder()
+                .totalElements(pages.getTotalElements())
+                .totalPages(pages.getTotalPages())
+                .content(trajectoryResponses)
+                .build();
+
+        return ResponseEntity.ok(response);
     }
 }
