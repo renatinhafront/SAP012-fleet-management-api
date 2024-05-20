9cfe76b17a8e9aa90356c67fa4d929bdabcfbdec
renatinhafront <re.saraiva09@hotmail.com>
2024-05-20 19:22:00 -0300

    add o método de buscar ultima trajetória

--- src/main/java/com/renatinha/fleetmanagement/services/TaxiService.java
+++ src/main/java/com/renatinha/fleetmanagement/services/TaxiService.java
@@ -1,6 +1,8 @@
 package com.renatinha.fleetmanagement.services;
 
+import com.renatinha.fleetmanagement.entities.LastTrajectory;
 import com.renatinha.fleetmanagement.entities.Taxi;
+import com.renatinha.fleetmanagement.repositories.LastTrajectoryRepository;
 import com.renatinha.fleetmanagement.repositories.TaxiRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,13 +13,19 @@ public class TaxiService {
 
     //cria atributo repository que faz a busca no banco
     public final TaxiRepository taxiRepository;
+    public final LastTrajectoryRepository lastTrajectoryRepository;
 
     //construtor
-    public TaxiService(final TaxiRepository taxiRepository) {
+    public TaxiService(final TaxiRepository taxiRepository, LastTrajectoryRepository lastTrajectoryRepository) {
         this.taxiRepository = taxiRepository;
+        this.lastTrajectoryRepository = lastTrajectoryRepository;
     }
 
     public Page<Taxi> getAllTaxis(Pageable pageable) {
         return taxiRepository.findAll(pageable);
+    }
+
+    public Page<LastTrajectory> getAllLastTrajectory(Pageable pageable) {
+        return lastTrajectoryRepository.findAllTaxisAndYoursLastTrajectory(pageable);
     }
 }
