96b194e9996d8c18e779231996c9ceda5cb7f976
renatinhafront <re.saraiva09@hotmail.com>
2024-05-20 11:30:07 -0300

    clear code

--- src/main/java/com/renatinha/fleetmanagement/repositories/TaxiRepository.java
+++ src/main/java/com/renatinha/fleetmanagement/repositories/TaxiRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 
 // spring data PagingAndSortingRepository = fornece métodos para fazer paginação e classificação de registros
 public interface TaxiRepository extends PagingAndSortingRepository<Taxi, Long> {
+
 }
